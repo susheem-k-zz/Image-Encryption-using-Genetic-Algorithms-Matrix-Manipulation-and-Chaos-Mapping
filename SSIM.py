26fc6ee3de4d9701e2ab5c666a32f29569675b3a
Anjan Das <dasanjan1296@gmail.com>
2019-10-25 15:23:07 +0530

    Update SSIM.py

--- SSIM.py
+++ SSIM.py
@@ -4,5 +4,6 @@ x='PartialPivot' #select the algorithm being tested
 im1=cv2.imread(r'test.jpg',0)
 im2=cv2.imread(r'output_'+x+'_decrypted.jpg',0)
 im3=cv2.imread(r'output_'+x+'.jpg',0)
+im4=cv2.imread(r'output_'+x+'.jpg',0)
 print(ssim(im1,im2))
 print(ssim(im1,im3))
